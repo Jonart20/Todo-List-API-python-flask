b092f7399fc1048505baf880f8f3ddef1db5d084
Jonart20 <jduran542@gmail.com>
2021-04-14 08:09:22 +0000

    final

--- src/main.py
+++ src/main.py
@@ -9,6 +9,17 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
+"""
+This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+"""
+import os
+from flask import Flask, request, jsonify, url_for
+from flask_migrate import Migrate
+from flask_swagger import swagger
+from flask_cors import CORS
+from utils import APIException, generate_sitemap
+from admin import setup_admin
+from models import db, User, Todos
 #from models import Person
 
 app = Flask(__name__)
@@ -39,6 +50,44 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@app.route('/get_todos', methods=['GET'])
+def get_tod():
+
+    # get all the favorites
+    query = Todos.query.all()
+
+    # map the results and your list of people  inside of the all_people variable
+    results = list(map(lambda x: x.serialize(), query))
+
+    return jsonify(results), 200
+
+@app.route('/add_todos', methods=['POST'])
+def add_tod():
+
+    # recibir info del request
+    request_body = request.get_json()
+    print(request_body)
+
+
+    tod = Todos(done=request_body["done"], label=request_body["label"])
+    db.session.add(tod)
+    db.session.commit()
+
+    return jsonify("All good"), 200
+
+@app.route('/del_todos/<int:tid>', methods=['DELETE'])
+def del_tod(tid):
+
+    # recibir info del request
+    
+    tod = Todos.query.get(tid)
+    if tod is None:
+        raise APIException('Label not found', status_code=404)
+
+    db.session.delete(tod)
+    db.session.commit()
+
+    return jsonify("All good"), 200
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
